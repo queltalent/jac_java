84a52602103be1dc14fdd121d07e55f6d951793f
MacBookPro <linnan.ca@gmail.com>
2023-11-29 18:35:49 -0500

    Last commit

--- src/MyProgram/PatternPrinter.java
+++ src/MyProgram/PatternPrinter.java
@@ -7,27 +7,26 @@ public class PatternPrinter {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner kb = new Scanner(System.in);
-		
-		int patternNumber, inputValue;
+		int patternNumber, inputRow;
 		boolean firstTime = true;
 		
         while (true) {
         	//The first and subsequent entries have different titles.
         	if(firstTime) {
-        		System.out.println(" - - - Welcome to Nancy's Triangle/Diamond Printer - - - ");
+        		System.out.println(" - - - Welcome to our Triangle/Diamond Printer - - - ");
                 System.out.println();
                 System.out.println("Which pattern do you want to print?");
         	}else {
-        		 System.out.println();
-        		 System.out.println("So which pattern do you want to print now？");
+        		System.out.println();
+        		System.out.println("So which pattern do you want to print now？");
         	}
         	
-        	//Print 4 pattern selection         	
-            System.out.println("1) 54321\t2)   1\t\t3) 12345\t4)  1");
-            System.out.println("   5432\t\t    12\t\t    2345\t   123");
-            System.out.println("   543\t\t   123\t\t     345\t  12345");
-            System.out.println("   54\t\t  1234\t\t      45\t   123");
-            System.out.println("   5\t\t 12345\t\t       5\t    1");
+        	//Display 4 pattern selection         	
+            System.out.println("1) 54321\t2)     1\t3) 12345\t4)   1");
+            System.out.println("   5432\t\t      12\t    2345\t    123");
+            System.out.println("   543\t\t     123\t     345\t   12345");
+            System.out.println("   54\t\t    1234\t      45\t    123");
+            System.out.println("   5\t\t   12345\t       5\t     1");
             System.out.println();
             System.out.print("Enter your choice (pattern number(1-4),5 to Quit): ");
          
@@ -46,43 +45,42 @@ public class PatternPrinter {
             //Determine whether the entered inputValue is valid（More than 1）          
             do{
             	System.out.print("How many rows would you like to print? (More than 1 please): ");
-                inputValue = kb.nextInt();
-              }while(inputValue <= 1);
+            	inputRow = kb.nextInt();
+              }while(inputRow <= 1);
             //Accord to patternNumber, call the corresponding program
             switch (patternNumber) {
                 case 1:
-                    printPattern1(inputValue);
+                    printPattern1(inputRow);
                     break;
                 case 2:
-                	printPattern2(inputValue);
+                	printPattern2(inputRow);
                     break;
                 case 3:
-                	printPattern3(inputValue);
+                	printPattern3(inputRow);
                     break;
                 case 4:
-                	printPattern4(inputValue);
+                	printPattern4(inputRow);
                     break;
                 default:
                     break;
-                    
             }
             firstTime = false;
         }
         kb.close();
     }
     //Generate printPattern1   	
-	private static void printPattern1(int value) {
-	    for (int i = 0; i < value; i++) {
-	        for (int j = value; j > i ; j--) {
+	private static void printPattern1(int row) {
+	    for (int i = 0; i < row; i++) {
+	        for (int j = row; j > i ; j--) {
 	            System.out.print(j);
 	        }
 	        System.out.println();
 	    }
 	}
     //Generate printPattern2
-	private static void printPattern2(int value) {
-		for (int i = 1; i <= value; i++) {
-	    	for (int j = 1; j <= value-i; j++) {
+	private static void printPattern2(int row) {
+		for (int i = 1; i <= row; i++) {
+	    	for (int j = 1; j <= row - i; j++) {
 	    		System.out.print(" ");
 	    	}
 	        for (int k = 1; k <=i ; k++) {
@@ -92,70 +90,48 @@ public class PatternPrinter {
 	    }
 	}
     //Generate printPattern3
-	private static void printPattern3(int value) {
-	    for (int i = value; i >=1; i--) {
-	    	for (int j = 1; j <= value-i; j++) {
+	private static void printPattern3(int row) {
+	    for (int i = row; i >=1; i--) {
+	    	for (int j = 1; j <= row - i; j++) {
 	    		System.out.print(" ");
 	    	}
-	        for (int k = value - i + 1; k <=value ; k++) {
+	        for (int k = row - i + 1; k <=row ; k++) {
 	            System.out.print(k);
 	        }
 	        System.out.println();
 	    }
 	}
     //Generate printPattern4	
-	private static void printPattern4(int value) {
+	private static void printPattern4(int row) {
 		// Determine whether the value is an odd or even number
-		if (value % 2 !=0) {
-			//value is an odd number
-			int midRow = (value + 1) / 2;
-			// Print upper half of the pattern
-	        for (int i = 1; i <= midRow; i++) {
-	            for (int j = 1; j <= midRow - i; j++) {
-	                System.out.print(" ");
-	            }
-	            for (int j = 1; j <= 2 * i - 1; j++) {
-	                System.out.print(j);
-	            }
-	            System.out.println();
-	        }
-	        // Print lower half of the pattern
-	        for (int i = midRow - 1; i >= 1; i--) {
-	            for (int j = 1; j <= midRow - i; j++) {
-	                System.out.print(" ");
-	            }
-	            for (int j = 1; j <= 2 * i - 1; j++) {
-	                System.out.print(j);
-	            }
-	            System.out.println();
-	        }
-		}else {
-			//value is an even number
-			int midRow = value / 2;
-			// Print upper half of the pattern
-	        for (int i = 1; i <= midRow; i++) {
-	            // Print spaces
-	            for (int j = 1; j <= midRow - i; j++) {
-	                System.out.print(" ");
-	            }
-	            for (int j = 1; j <= 2 * i - 1; j++) {
-	                System.out.print(j);
-	            }
-	            System.out.println();
-	        }
-	        // Print lower half of the pattern
-	        for (int i = midRow; i >= 1; i--) {
-	            for (int j = 1; j <= midRow - i; j++) {
-	                System.out.print(" ");
-	            }
-	            for (int j = 1; j <= 2 * i - 1; j++) {
-	                System.out.print(j);
-	            }
-	            System.out.println();
-	        }
+		int midRow;
+		// Determine whether the value is an odd or even number
+		if (row % 2 !=0) {
+			midRow = (row + 1) / 2;
+		}else {			
+			midRow = row / 2;
 		}
 		
+		// Print upper half of the pattern
+        for (int i = 1; i <= midRow; i++) {
+            for (int j = 1; j <= midRow - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
         
+        // Print lower half of the pattern
+        for (int i = row - midRow; i >= 1; i--) {
+            for (int j = 1; j <= midRow - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }   
 	}
-
 }
